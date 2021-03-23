e0ffa4bcdefd16a1b032ee75e6f901ea21331ac1
nonusae <nonusae@gmail.com>
2021-03-24 00:55:38 +0700

    Add PhotoContext

---
+++ src/Context.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from 'react'
+
+const Context = createContext()
+
+const ContextProvider = ({children}) => {
+  const [allPhotos, setAllPhotos] = useState([])
+
+  return (
+    <Context.Provider value={{allPhotos}}>
+      {children}
+    </Context.Provider>
+  )
+}
+
+export { ContextProvider, Context }
