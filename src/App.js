80cac931ae2f9be7c96530a5533357c1303afbc5
nonusae <nonusae@gmail.com>
2021-03-23 23:02:03 +0700

    Add Router and Link

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from "react"
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
+import { Switch, Route } from "react-router-dom"
 
-function App() {
+const App = () => {
     return (
-        <div>
-            <Header />
-            <h1>Home Page</h1>
-        </div>
+      <div>
+          <Header />
+
+          <Switch>
+            <Route exact path='/'>
+              <Photos />
+            </Route>
+            <Route path='/cart'>
+              <Cart />
+            </Route>
+          </Switch>
+      </div>
     )
 }
+
 
 export default App
