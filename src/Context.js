47e0c7f5d62dbedc478fcacd58b9725f4b842fb9
nonusae <nonusae@gmail.com>
2021-03-24 01:24:29 +0700

    Implement function for fetch photo data

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,21 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 
 const Context = createContext()
+const URL = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
 
 const ContextProvider = ({children}) => {
   const [allPhotos, setAllPhotos] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(URL)
+      const data = await response.json()
+
+      setAllPhotos(data)
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <Context.Provider value={{allPhotos}}>
--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,6 +1,10 @@
-import React from "react"
+import React, {useContext} from "react"
+import {Context} from '../Context'
 
 function Photos() {
+    const data = useContext(Context)
+    console.log(data)
+
     return (
         <main className="photos">
             <h1>Images go here</h1>
