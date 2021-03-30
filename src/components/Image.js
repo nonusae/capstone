aaf6833987d49fcfd19ae5ed8bf682b8099b0f61
nonusae <nonusae@gmail.com>
2021-03-30 19:53:25 +0700

    Implenent added image state display for cart icon

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,7 @@ import { Context } from '../Context'
 
 function Image({className, img}) {
   const [hovered, setHovered] = useState(false)
-  const { toggleFavorite, addItemToCart } = useContext(Context)
+  const { toggleFavorite, addItemToCart, cartItems } = useContext(Context)
 
   const heartIcon = () => {
     if (img.isFavorite) {
@@ -13,11 +13,13 @@ function Image({className, img}) {
     }
   }
 
-  const cartIcon = hovered
-    && <i
-          className="ri-add-circle-line cart"
-          onClick={() => addItemToCart(img)}
-        />
+  const cartIcon = () => {
+    if (cartItems.some(item => item === img)) {
+      return <i className="ri-shopping-cart-fill cart"></i>
+    } else {
+      return hovered && <i className="ri-add-circle-line cart" onClick={() => addItemToCart(img)}/>
+    }
+  }
 
   return (
     <div
@@ -27,7 +29,7 @@ function Image({className, img}) {
     >
       <img className="image-grid" src={img.url} alt=""/>
       {heartIcon()}
-      {cartIcon}
+      {cartIcon()}
     </div>
   )
 }
